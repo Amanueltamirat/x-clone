5b9e52cef2c5bfdd08413ae81966b29bf146b4c5
https://github.com/Amanueltamirat <amanuel.tamirat22@gmail.com>
2024-10-06 00:17:16 +0300

    ready!

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import authRoute from './routes/auth.routes.js'
 import dotenv from 'dotenv'
 import mongoDbConnection from './db/db.js'
@@ -20,6 +21,8 @@ cloudinary.config({
 
 const PORT = process.env.PORT || 5000
 const app = express()
+const __dirname = path.resolve()
+
 app.use(express.urlencoded({extended:true}))
 app.use(cors());
 app.use(express.json({limit:'5mb'}))
@@ -29,7 +32,16 @@ app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/posts', postRoute);
 app.use('/api/notification', notificationRoute)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
 app.listen(PORT,()=>{
-    console.log('Listening at port 8008')
+    console.log('Listening at port:',PORT)
     mongoDbConnection()
 })
